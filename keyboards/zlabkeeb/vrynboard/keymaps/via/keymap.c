f50f235bfd913032a12f5d962cf9747c38b64106
zlabkeeb <160311066+zlabkeeb@users.noreply.github.com>
2024-10-08 17:47:11 +0000

    UPDATE KEYMAP

--- keyboards/zlabkeeb/vrynboard/keymaps/via/keymap.c
+++ keyboards/zlabkeeb/vrynboard/keymaps/via/keymap.c
@@ -19,10 +19,31 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [0] = LAYOUT(
+     [0] = LAYOUT(
         QK_KB_0,
         KC_VOLD,   KC_MUTE,   KC_VOLU,
         KC_LEFT,   KC_MPLY,   KC_RGHT,
         KC_ESC,    KC_PSCR,   KC_DEL
+    ),
+
+    [1] = LAYOUT(
+        QK_KB_0,
+        KC_1,   KC_2,   KC_3,
+        KC_4,   KC_6,   KC_7,
+        KC_TRNS,   KC_TRNS,   KC_TRNS
+    ),
+
+    [2] = LAYOUT(
+        QK_KB_0,
+        KC_J,   KC_K,   KC_L,
+        KC_M,   KC_N,   KC_O,
+        KC_TRNS,   KC_TRNS,   KC_TRNS
+    ),
+
+    [3] = LAYOUT(
+        QK_KB_0,
+        UG_NEXT,   UG_TOGG,  UG_PREV,
+        UG_HUED,   KC_ESC,   UG_HUEU,
+        UG_VALD,   KC_ENT,   UG_VALU
     )
 };
