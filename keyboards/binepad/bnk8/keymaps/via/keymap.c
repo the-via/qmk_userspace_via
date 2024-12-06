5187fb1efa8ac031a280c72a2cf8d9f352b7f2ff
Vino Rodrigues <366673+vinorodrigues@users.noreply.github.com>
2024-12-06 12:52:23 +1100

    RGB_ -> RM_ keycodes after Nov'24 breaking changes

--- keyboards/binepad/bnk8/keymaps/via/keymap.c
+++ keyboards/binepad/bnk8/keymaps/via/keymap.c
@@ -19,9 +19,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P6,    KC_P7,    LT(1, KC_P8)
     ),
     [1] = LAYOUT_ortho_3x3(
-        RGB_HUI,  RGB_SAI,  RGB_SPI,
-        RGB_HUD,  RGB_SAD,  RGB_SPD,
-        RGB_TOG,  RGB_MOD,  _______
+        RM_HUEU, RM_SATU, RM_SPDU,
+        RM_HUED, RM_SATD, RM_SPDD,
+        RM_TOGG, RM_NEXT, _______
     )
 };
 
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(RGB_VAD, RGB_VAI) }
+    [1] = { ENCODER_CCW_CW(RM_VALD, RM_VALD) }
 };
 
 #endif
