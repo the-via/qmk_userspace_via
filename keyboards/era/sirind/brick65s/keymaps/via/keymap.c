43dc01d54619ba0dea01a639573adbd559c05422
era <phyojinbin@gmail.com>
2024-12-24 18:00:19 +0900

    Add Brick65S
    Add Brick65S

--- keyboards/era/sirind/brick65s/keymaps/via/keymap.c
+++ keyboards/era/sirind/brick65s/keymaps/via/keymap.c
@@ -6,19 +6,19 @@
 #include "eeprom.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_ESC,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSLS,   KC_BSPC,   KC_DEL, 
-        KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,             KC_BSLS,   KC_LSCR, 
-        KC_CAPS,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,         
+    [0] = LAYOUT_all(
+        KC_ESC,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_BSLS,   KC_BSPC,   KC_INS,
+        KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC,             KC_BSLS,   KC_DEL,
+        KC_CAPS,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,
         KC_LSFT,              KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,             KC_UP, 
-        KC_LCTL,    KC_LWIN,  KC_LALT,                   KC_SPC,           MO(1),            KC_SPC,  KC_RALT, KC_RCTL,            KC_LEFT,   KC_DOWN,   KC_RGHT
+        KC_LCTL,    KC_LWIN,  KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),              KC_LEFT,   KC_DOWN,   KC_RGHT
     ),
-    [1] = LAYOUT(
-        KC_TILD,    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_TRNS,   KC_TRNS,   KC_TRNS, 
-        KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,             KC_TRNS,   KC_TRNS, 
+    [1] = LAYOUT_all(
+        KC_TILD,    KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12,   KC_TRNS,   KC_TRNS,   KC_PGUP,
+        KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SCRL,  KC_PAUS,             KC_TRNS,   KC_PGDN,
         KC_TRNS,    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS,         
-        KC_TRNS,              RGB_TOG, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE,  KC_TRNS,             KC_TRNS, 
-        KC_TRNS,    KC_TRNS,  KC_TRNS,                   KC_TRNS,          KC_TRNS,          KC_TRNS, RGB_MOD, KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS
+        KC_TRNS,              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLU, KC_VOLD, KC_TRNS,  KC_TRNS,             KC_TRNS, 
+        KC_TRNS,    KC_TRNS,  KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS,            KC_TRNS,   KC_TRNS,   KC_TRNS
     )
 };
 
@@ -95,7 +95,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max)
     return false;
 }
 
-#ifdef VIA_ENABLE
 void via_init_kb(void)
 {
     // If the EEPROM has the magic, the data is good.
