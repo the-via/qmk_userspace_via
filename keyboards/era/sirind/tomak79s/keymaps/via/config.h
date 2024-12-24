5ac8a005b9b05e5f96a153165da553208feb3116
era <phyojinbin@gmail.com>
2024-12-24 09:32:23 +0900

    Add Custom UI code
    Add Custom UI code

---
+++ keyboards/era/sirind/tomak79s/keymaps/via/config.h
@@ -0,0 +1,8 @@
+// Copyright 2024 Hyojin Bak (@eerraa)
+// SPDX-License-Identifier: GPL-2.0-or-later
+ 
+#pragma once
+
+/* Sync configuration */
+#define TOMAK_CONFIG_SYNC
+#define SPLIT_TRANSACTION_IDS_KB RPC_ID_KB_CONFIG_SYNC
