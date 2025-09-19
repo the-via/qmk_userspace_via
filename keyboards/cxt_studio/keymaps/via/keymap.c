a5578d734cc8bc9f2dbfc62163ac9225e15cd519
Mari <marina.rubinato@icloud.com>
2025-09-19 16:15:41 -0300

    Add files via upload

--- keyboards/cxt_studio/keymaps/via/keymap.c
+++ keyboards/cxt_studio/keymaps/via/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_MRWD, KC_MFFD,
         KC_NO, KC_MPLY, KC_MPLY, KC_MNXT,
 
-        KC_MUTE, KC_NO, KC_NO, RGB_TOG
+        KC_MUTE, KC_NO, KC_NO, RGB_M_G
     )
 };
 
@@ -30,9 +30,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = {
         ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
-        ENCODER_CCW_CW(RGB_HUD, RGB_HUI),
-        ENCODER_CCW_CW(RGB_VAD, RGB_VAI),
-        ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU),
+        ENCODER_CCW_CW(KC_VOLD, KC_VOLU)
     },
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    uint8_t index_to_light = NO_LED;
+
+    switch (get_highest_layer(layer_state | default_layer_state)) {
+        case 1:
+            index_to_light = 3;
+            break;
+        case 2:
+            index_to_light = 2;
+            break;
+        case 3:
+            index_to_light = 1;
+            break;
+        case 4:
+            index_to_light = 0;
+            break;
+        default:
+            break;
+    }
+
+    // Light up only the selected LED if it's within the current render range
+    if (index_to_light != NO_LED && index_to_light >= led_min && index_to_light < led_max) {
+        rgb_matrix_set_color(index_to_light, RGB_GREEN); // You can choose any color
+    }
+
+    return false;  // Let other lighting effects (if any) still apply
+}
